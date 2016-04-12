5eb60f0943d1127836539e45eeb5c464e62f3232
Kriz Anthony Mayo <k3mayo@gmail.com>
2016-04-12 14:08:55 -0400

    added question view controllers

---
+++ TutorME/TutorME/QuestionViewController.h
@@ -0,0 +1,16 @@
+//
+//  QuestionViewController.h
+//  TutorME
+//
+//  Created by kmayo on 2016-04-12.
+//  Copyright © 2016 kmayo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionViewController : UITableViewController <UITextViewDelegate>
+{
+    
+}
+
+@end
---
+++ TutorME/TutorME/QuestionViewController.m
@@ -0,0 +1,26 @@
+//
+//  QuestionViewController.m
+//  TutorME
+//
+//  Created by kmayo on 2016-04-12.
+//  Copyright © 2016 kmayo. All rights reserved.
+//
+
+#import "QuestionViewController.h"
+
+@interface QuestionViewController ()
+
+@end
+
+@implementation QuestionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
