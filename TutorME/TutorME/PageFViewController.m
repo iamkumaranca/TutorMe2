c1887737615007230021ec11096718b688c25ca8
iOS Xcode User <xcode@FASTs-MacBook-Pro-8.local>
2016-04-17 20:50:27 -0400

    About Page

---
+++ TutorME/TutorME/PageFViewController.m
@@ -0,0 +1,44 @@
+//
+//  PageFViewController.m
+//  TutorME
+//
+//  Created by iOS Xcode User on 2016-04-17.
+//  Copyright © 2016 kmayo. All rights reserved.
+//
+
+#import "PageFViewController.h"
+
+@interface PageFViewController ()
+
+@end
+
+@implementation PageFViewController
+@synthesize wbPage, activity;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSURL *urlAddress = [NSURL URLWithString:@"https://www.facebook.com"];
+    NSURLRequest *url = [NSURLRequest requestWithURL:urlAddress];
+    [wbPage loadRequest:url];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)exit:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
