ef14e40d0921468e1f30a89ebc37ca303073c001
Kriz Anthony Mayo <k3mayo@gmail.com>
2016-04-22 17:15:47 -0400

    remove firebase ref on about not needed

--- TutorME/TutorME/AboutViewController.m
+++ TutorME/TutorME/AboutViewController.m
@@ -8,20 +8,18 @@
 #import "AboutViewController.h"
 
 @interface AboutViewController ()
-@property (strong, nonatomic) Firebase *ref;
+
 @end
 
 @implementation AboutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Initialize Firebase reference
-    self.ref = [[Firebase alloc] initWithUrl:@"https://burning-heat-7302.firebaseio.com/"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 @end
